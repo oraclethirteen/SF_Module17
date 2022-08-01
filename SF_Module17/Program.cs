a65dfa70014d7f19dfce24a461b6dad79ed5da5b
oraclethirteen <oracle.thirteen.o@gmail.com>
2022-08-01 19:18:56 +0300

    Update

--- SF_Module17/Program.cs
+++ SF_Module17/Program.cs
@@ -4,9 +4,68 @@ namespace SF_Module17
 {
     class Program
     {
+        // [17.1.5]
+        /*
+        public static class Constants
+        {
+            public const string WelcomeString = "Welcome to the forecast page for the";
+            public const string ForecastString = "The average temperature expects to be around ";
+            public const string LineBreak = "\n";
+        }
+        */
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // [17.1.5]
+            /*
+            DailyAverage();
+            WeeklyAverage();
+            MonthlyAverage();
+            */
         }
+
+        // [17.1.4]
+        /*
+        public String GetDayOfWeek2(int day)
+        {
+            switch (day)
+            {
+                case 1:
+                    return "Monday";
+                case 2:
+                    return "Tuesday";
+                case 3:
+                    return "Wednesday";
+                case 4:
+                    return "Thursday";
+                case 5:
+                    return "Friday";
+                case 6:
+                    return "Saturday";
+                case 7:
+                    return "Sunday";
+                default:
+                    throw new InvalidOperationException("День недели должен быть в диапазоне 1 to 7");
+            }
+        }
+        */
+
+        // [17.1.5]
+        /*
+        private static void DailyAverage()
+        {
+            Console.WriteLine($"{Constants.WelcomeString} day {Constants.LineBreak}{Constants.ForecastString} {new Random().Next(10, 30)} {Constants.LineBreak}");
+        }
+
+        private static void WeeklyAverage()
+        {
+            Console.WriteLine($"{Constants.WelcomeString} week {Constants.LineBreak}{Constants.ForecastString} {new Random().Next(10, 30)} {Constants.LineBreak}");
+        }
+
+        private static void MonthlyAverage()
+        {
+            Console.WriteLine($"{Constants.WelcomeString} month {Constants.LineBreak}{Constants.ForecastString} {new Random().Next(10, 30)} {Constants.LineBreak}");
+        }
+        */
     }
 }
